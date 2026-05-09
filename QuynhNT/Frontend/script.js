4bc43fb54231cd89557d01c2db09e9aad1bfcbe1
QuynhNT-1 <quynhnguyenattt@gmail.com>
2026-05-09 22:38:02 +0700

    QuynhNT_Bai tap 2

---
+++ QuynhNT/Frontend/script.js
@@ -0,0 +1,46 @@
+
+let name = "Nguyen Thi Quynh";
+let current = 2026;
+let birth = 1998;
+let isstudent = false
+let high = 150
+let hobby = "Chơi";
+let age = current - birth;
+
+
+
+console.log("Tên:",  name);
+console.log("Tuổi:",  age);
+console.log("là sinh viên:", isstudent);
+console.log("Chiều cao:",high + " cm");
+console.log("Sở thích:", hobby);
+
+console.log("Tên:" + name);
+console.log("Tuổi:"  + age);
+console.log("là sinh viên:" + isstudent);
+console.log("Chiều cao:" + high + " cm");
+console.log("Sở thích:" + hobby);
+
+
+console.log("................") 
+let user = {
+    name: "Nguyen Thi Quynh",
+    age: 28,
+    isstudent: false,
+    high: 150,
+    hobby: "Chơi"       
+};
+////khai báo kiểu mảng
+console.log("Tên:", user.name);
+console.log("Tuổi:", user.age);
+console.log("là sinh viên:", user.isstudent);
+console.log("Chiều cao:", user.high + " cm");
+console.log("Sở thích:", user.hobby);
+
+/// kiểm tra kiểu dữ liệu
+console.log(typeof name);
+console.log(typeof age); 
+
+
+
+
