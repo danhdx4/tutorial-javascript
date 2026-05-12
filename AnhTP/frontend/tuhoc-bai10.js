b48807b3b0efa89e5a01c3e87b945b68126db19d
VPHNILA0108\user <phuonganh.hn87@gmail.com>
2026-05-12 15:07:19 +0700

    atp bài tập buổi 3

---
+++ AnhTP/frontend/tuhoc-bai10.js
@@ -0,0 +1,14 @@
+let a = 99;
+let b = 10;
+let c = 77;
+let d = 88;
+
+a++; // Toán tử tăng (increment)
+b--;
+++c;
+--d;
+
+console.log("Giá trị của a sau khi tăng: " + a); // Kết quả: 100
+console.log("Giá trị của b sau khi giảm: " + b); // Kết quả: 9
+console.log("Giá trị của c sau khi tăng: " + c); // Kết quả: 78
+console.log("Giá trị của d sau khi giảm: " + d); // Kết quả: 87
