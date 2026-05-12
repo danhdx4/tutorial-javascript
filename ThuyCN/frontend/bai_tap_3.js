bb4cdb5093fe76255083c8ddd945710dad9c3c7c
DESKTOP-F37KTNS\NgocThuy <caongocthuy999@gmail.com>
2026-05-12 23:06:11 +0700

    ThuyCN nộp bài tập về nhà buổi 3

---
+++ ThuyCN/frontend/bai_tap_3.js
@@ -0,0 +1,43 @@
+// Tạo các biến:​
+// Họ tên (string), ​
+// Tuổi (number, và tính tuổi =(2026 - năm sinh)​
+// Số năm kinh nghiệm (number) ​
+// Có biết automation hay không (boolean) 
+
+let name = "Cao Ngoc Thuy";
+let birth = 1999;
+let age = 2026 - birth;
+let experience = 3;
+let isAuto = true;
+
+// Phân loại 
+// < 1 năm → "Fresher"
+// 1–3 năm → "Junior QA"
+// 3–5 năm → "Middle QA"
+// >5 năm → "Senior QA"
+
+if (experience < 1) {
+console.log("Fresher")
+} else if (experience <= 3) {
+console.log("Junior QA")
+} else if (experience <= 5) {
+console.log("Middle QA")
+} else {
+console.log("Senior QA")
+}
+
+// Kiểm tra điều kiện tuyển Senior Automation QA 
+//    + Kinh nghiệm ≥ 3 năm
+//    + Biết automation = true
+//    + Tuổi ≤ 35
+// Nếu thỏa mãn các yêu cầu trên:→ "Đủ điều kiện Senior Automation QA"
+// Ngược lại:→ "Chưa đủ điều kiện“
+// In toàn bộ thông tin
+
+if (experience >= 3 && isAuto == true && age <= 35) {
+console.log("Đủ điều kiện Senior Automation QA")
+} else {
+console.log("Chưa đủ điều kiện")
+}
+
+
