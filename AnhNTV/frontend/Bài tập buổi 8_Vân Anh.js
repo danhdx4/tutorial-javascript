28f9b3ded8d90a29418c9863a3ffc2fe94cbc14e
Van Anh <vananh217.tm@gmail.com>
2026-05-31 17:18:33 +0700

    no message

--- AnhNTV/frontend/Bài tập buổi 8_Vân Anh.js
+++ AnhNTV/frontend/Bài tập buổi 8_Vân Anh.js
@@ -62,9 +62,96 @@ console.log(dog2.speak());
 // Method getTestInfo() 
 // → in "Tester <username> đang test trên <testEnv>"
 console.log("Bài 4: Class TestUser.............");
+
 class User {
   constructor(id, username, role) {
     this.id = id;
     this.username = username;
     this.role = role;
   }
+
+  getProfile() {
+    return `ID: ${this.id}, Username: ${this.username}, Role: ${this.role}`;
+  }
+}
+
+class TestUser extends User {
+  constructor(id, username, testEnv) {
+    super(id, username, "tester");
+    this.testEnv = testEnv;
+  }
+
+  getTestInfo() {
+    return `Tester ${this.username} đang test trên ${this.testEnv}`;
+  }
+}
+
+// test bài 4
+const tester1 = new TestUser(
+  101,
+  "QA_AnhNTV",
+  "staging"
+);
+
+console.log(tester1.getProfile());
+console.log(tester1.getTestInfo());
+
+
+// Bài luyện thêm 1 — Class Vehicle (cha)
+// Constructor: brand, maxSpeed → gán vào this
+// Method getInfo() dùng template literal in "<brand> — tốc độ tối đa <maxSpeed> km/h"
+// Test thử getInfo(): new Vehicle("Toyota", 180)
+// Kết quả mong đợi: Toyota — tốc độ tối đa 180 km/h
+
+console.log("Bài luyện thêm 1 : Class Vehicle.............");
+class Vehicle {
+  constructor (brand, maxSpeed){
+  this.brand = brand;
+  this.maxSpeed = maxSpeed;
+  }
+  getInfo() {
+    return `${this.brand} — tốc độ tối đa ${this.maxSpeed} km/h`;
+  }
+}
+
+//test
+const vehicle = new Vehicle ("Toyota", 180);
+console.log(vehicle.getInfo());
+
+// Bài 2 — Class Car kế thừa Vehicle
+// Dùng class Car extends Vehicle
+// Constructor nhận brand, maxSpeed, seats
+// Gọi super(brand, maxSpeed) rồi gán this.seats = seats.
+// Tạo method riêng honk() → in "<brand> bíp bíp 🚗"
+// Test thử: new Car("Honda", 160, 5)
+// Kết quả mong đợi: Honda bíp bíp 🚗
+
+console.log("Bài luyện thêm 2,3 : Class Car kế thừa Vehicle, Override getInfo().............");
+class Car extends Vehicle {
+constructor (brand, maxSpeed, seats){
+  super(brand, maxSpeed);
+  this.seats = seats;
+}
+  hock() {
+    return`${this.brand} bíp bíp 🚗`
+  }
+
+  getInfo() {
+    return`${this.brand} — ${this.maxSpeed} km/h — ${this.seats} chỗ`
+  }
+}
+
+const car = new Car ("Honda", 160, 5);
+console.log(car.getInfo());
+console.log(car.hock());
+
+const car2 = new Car("Mazda", 200, 4);
+console.log(car2.getInfo());
+
+
+// Bài 3 — Override getInfo()
+// Trong Car, override getInfo() để in thêm số ghế
+// Gợi ý: return `${this.brand} — ${this.maxSpeed} km/h — ${this.seats} chỗ`;
+// Test thử: const car = new Car("Mazda", 200, 4); car.getInfo();
+// Kết quả mong đợi: Mazda — 200 km/h — 4 chỗ
+
