f5f0dcedcc5d4b464abb4c3ad183e2aa3df25743
Đặng Xuân Danh <danhdx@Dang-Xuan-Danh.local>
2026-06-03 17:08:33 +0700

    lesson 8

---
+++ danhdx/backend/models/index.js
@@ -0,0 +1,43 @@
+const Product = require("./Product");
+const { showLog } = require("./function")
+
+const pro = new Product(1, "Quan", 120000);
+
+const productJson = require("../data/products.json");
+
+
+
+
+
+
+// async function layDuLieu() {
+
+//   console.log("Bắt đầu gọi API...");
+
+//   // "await" = chờ kết quả trước khi tiếp tục
+//   const response = await fetch(
+//     "https://products-api-ten-alpha.vercel.app/api/products"
+//   );
+
+//   const data = await response.json();
+// console.log(data)
+
+// //   console.log(`Số sản phẩm: ${data.length}`);
+// }
+
+// Gọi function async
+// layDuLieu();
+
+
+
+async function layChiTiet(id) {
+  try{
+    "23bfjsdbf";
+    s
+    console.log(1);
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+layChiTiet(2);
