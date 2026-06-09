1082d67c589c9dee5d9f45569a086780f368af31
Van Anh <vananh217.tm@gmail.com>
2026-06-09 16:36:43 +0700

    no message

--- AnhNTV/File_thua/Web-auto/demo-test/tests/example.spec.ts
+++ AnhNTV/File_thua/Web-auto/demo-test/tests/example.spec.ts
@@ -16,3 +16,8 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test.only('test-app', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+});
+ 
