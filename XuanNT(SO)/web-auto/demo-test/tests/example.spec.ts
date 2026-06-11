369ab3b28e9a897e4a27ea0f0ee6ba2f96358895
ntxuan2003 <xuannguyen2003ha@gamil.com>
2026-06-11 08:00:26 +0700

    no message

--- XuanNT(SO)/web-auto/demo-test/tests/example.spec.ts
+++ XuanNT(SO)/web-auto/demo-test/tests/example.spec.ts
@@ -8,11 +8,7 @@ test.only('has title', async ({ page }) => {
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://localhost:4200/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  
 });
