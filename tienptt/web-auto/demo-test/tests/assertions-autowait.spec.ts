452be2dbe589433fc3e0d276baf4ac0543bda971
ADMIN-PC\Admin <tienptt1998@gmail.com>
2026-06-16 19:27:23 +0700

    tienptt commit btvn b4

---
+++ tienptt/web-auto/demo-test/tests/assertions-autowait.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from '@playwright/test';
+
+
+
+test ('test verify basic Form and Submit', async({page})=>{
+  // đi tới link
+  await page.goto("http://localhost:4200/");
+
+  // click forms trên menu
+  await page.getByText("Forms").click();
+
+  // click form layout
+    await page.getByText("Form Layouts").click();
+  
+    // xác định basic form
+    const basicform= page.locator('nb-card').filter({ hasText: 'basic form'});
+const fieldemail = basicform.getByRole('textbox', { name: 'Email',
+  });
+  const fieldpassword = basicform.locator('input[type="password"]');
+  const submitButton = basicform.getByRole('button', { name: 'Submit',
+  });
+//********8Verify ***********
+  // Placeholder Email
+  await expect(fieldemail).toHaveAttribute('placeholder', 'Email');
+
+  // Placeholder Password
+  await expect(fieldpassword).toHaveAttribute('placeholder', 'Password');
+    // Verify màu của nút Submit
+  await expect(submitButton).toHaveCSS(
+    'background-color',
+    'rgb(255, 61, 113)'
+  );
+
+  //  Nhập Email và Password
+  await fieldemail.fill('tienptt1998@gmail.com');
+  await fieldpassword.fill('123456');
+
+  // Verify dữ liệu đã nhập
+  await expect(fieldemail).toHaveValue('tienptt1998@gmail.com');
+  await expect(fieldpassword).toHaveValue('123456');
+
+  // Hoặc dùng extracting values:
+  // const emailValue = await fieldemail.inputValue();
+  // expect(emailValue).toEqual('tienptt1998@gmail.com');
+
+  // const passwordValue = await fieldemail.inputValue();
+  // expect(passwordValue).toEqual('123456');
+
+  // 7. Click Submit
+  await submitButton.click();
+})
+
