e94063bec10249225b034adb063756ac1a60e75d
ADMIN-PC\Admin <tienptt1998@gmail.com>
2026-06-24 21:01:31 +0700

    tienptt commit btvn buổi 6

--- tienptt/web-auto/demo-test/tests/btvnB6.spec.ts
+++ tienptt/web-auto/demo-test/tests/btvnB6.spec.ts
@@ -7,7 +7,7 @@ test('BTVN 6 - Tìm và xóa row có ID = 11', async ({ page }) => {
   await page.goto('http://localhost:4200/pages/tables/smart-table');
 // action > để chuyển trang tiếp theo
   const nextBtn = page.locator('.ng2-smart-pagination-nav').getByText('>');
-
+// Biến đánh dấu đã tìm thấy hay chưa
   let found = false;
 
   while (!found) {
@@ -16,7 +16,7 @@ test('BTVN 6 - Tìm và xóa row có ID = 11', async ({ page }) => {
     });
 
     console.log('Số dòng tìm thấy:', await targetRowById.count());
-
+git status
     if (await targetRowById.count() > 0) {
       found = true;
 
