0ed5e48af4474d8d184d45ed0f04f8d139857657
ththu13 <ngothuy13102003@gmail.com>
2026-06-29 21:38:06 +0700

    BTVN7_ThuyNT

---
+++ ThuyNT/demo-test/tests/datepicker.spec.ts
@@ -0,0 +1,40 @@
+import { expect, test } from '@playwright/test';
+
+test("date picket", async ({ page }) => {
+    await page.goto('http://localhost:4200/pages/forms/datepicker');
+
+    const formRange = page.getByPlaceholder("Range Picker");
+    await formRange.click();
+
+    let date = new Date();
+
+    const expectedDate = date.getDate().toString();
+    const expectedMonthShort = date.toLocaleString("en-US", { month: "short" });
+    const expectedMonthLong = date.toLocaleString("en-US", { month: "long" });
+    const expectedYear = date.getFullYear();
+    const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`;
+    const expectedMonthAndYear = `${expectedMonthLong} ${expectedYear}`;
+
+    await page
+        .locator('[class="range-cell day-cell today ng-star-inserted"]')
+        .getByText(expectedDate, { exact: true })
+        .click();
+
+    let endDate = new Date();
+    endDate.setDate(endDate.getDate() + 5);
+
+     const expectedEndDate = endDate.getDate().toString();
+    const expectedEndMonthShort = endDate.toLocaleString("en-US", { month: "short" });
+    const expectedEndMonthLong = endDate.toLocaleString("en-US", { month: "long" });
+    const expectedEndYear = endDate.getFullYear();
+    const dateToAssertEnd = `${expectedEndMonthShort} ${expectedEndDate}, ${expectedEndYear}`;
+    if (date.getMonth() === endDate.getMonth() && date.getFullYear() === endDate.getFullYear()) {
+        await page.locator('.day-cell').getByText(expectedEndDate, { exact: true }).click();
+    } else {
+        const btnNext = page.locator('.next-month');
+        await btnNext.click();
+        await page.locator('.day-cell').getByText(expectedEndDate, { exact: true }).click();
+    }
+
+    await expect(formRange).toHaveValue(dateToAssert + " - " + dateToAssertEnd);
+})
