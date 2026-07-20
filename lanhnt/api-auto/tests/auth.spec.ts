9f7110cc707aa910fe839af736be2c1838144ad9
LanhNguyen <lanh.thp@gmail.com>
2026-07-20 23:41:23 +0700

    LanhNT - bài 13 prepare

--- lanhnt/api-auto/tests/auth.spec.ts
+++ lanhnt/api-auto/tests/auth.spec.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { expect, test } from "@playwright/test";
-
-test.beforeEach(async ({ page }) => {
-    await page.goto("https://conduit.bondaracademy.com/");
-
-    // login with the credentical
-    await page.getByRole('link', { name: ' Sign in ' }).click()
-    await page.getByPlaceholder('Email').fill('lanh.zensho@test.com')
-    await page.getByPlaceholder('Password').fill('123456789')
-    await page.getByRole('button', { name: ' Sign in ' }).click()
-});
-
-test('Should be create a articel successfully', async ({ page, request }) => {
-    // todo
-    
-
-})
-
-test('Should delete the article successfully', async ({ page, request }) => {
-    // todo
-})
-=======
 import { test as setup } from "@playwright/test";
 import * as fs from 'fs';
 import * as path from 'path';
@@ -43,4 +20,3 @@ setup("Should be create a articel successfully", async ({ page, request }) => {
     // Ghi đối tượng JSON vào file
     fs.writeFileSync(filePath, JSON.stringify(responseBody));
 });
->>>>>>> 94e0fe18162e7fc222a412cee829587f5d81e907
